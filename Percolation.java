7f44a772ca8452de5c1ac8e3cf87cad4145ff7ed
jakejenn21 <47189949+jakejenn21@users.noreply.github.com>
2019-01-30 14:15:02 -0700

    Update Percolation.java

--- Percolation.java
+++ Percolation.java
@@ -1,180 +1,163 @@
 package a01;
-/**
- * @author - Annie Ruiz
- * This program uses Monte Carlo's simulation to estimate the value of percolation
- * threshold given a N-by-N grid */
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-
-	//METHODS
-	private boolean[][] isGridSiteOpen;
-	private WeightedQuickUnionUF quickUnion;
-	private WeightedQuickUnionUF backWash;
-	private final int LENGTH;
-	private int totSites;
-	private int openSites; // keeps track of open sites
-	private static int virtualTop;
-	private static int virtualBottom;
-
+	
+	private boolean[][] grid;
+	private WeightedQuickUnionUF uf;
+	private WeightedQuickUnionUF backwash;
+	
+	
+	private final int VIRTUAL_TOP; // virtual top-site
+	private final int VIRTUAL_BOTTOM;   // virtual bottom-site
+    private int gridSize;
+    private int numOfOpenSites ;
 	/**
-	 * Creates N-by-N grid, with all sites blocked Has two extra nodes as the
-	 * virtual top and the virtual bottom These are used to more easily determine
-	 * the percolation state
-	 * 
-	 * @param N: the size of the grid
-	 * @throws IllegalArgumentException if N <= 0
+	 * Creates NbyN grid, with all sites blocked.
+	 * @param N size of the grid
 	 */
 	public Percolation(int N) {
-		// throws exception if an invalid number is given
-		if (N <= 0) throw new IllegalArgumentException("Given N is less than or equal to 0 (Percolation())");
-
-		LENGTH = N; 							// assigning the length to N
-		totSites = N * N; 						// assigning the total sites to N-by-N
-		openSites = 0;							//assigns openSites to 0, since there's no open sites
-		isGridSiteOpen = new boolean[N][N]; 				// creates a 2D grid for the sites, immediately initializes to false
-		virtualTop = totSites + 1; 					// adds one extra site to the grid
-		virtualBottom = totSites + 2; 					// adds a second extra site to the grid
 		
-	/* quickUnion WeightedQuickUnionUF will be attached to the virtualBottom 
-	* and virtualTop sites and will check if the site percolates, the backWash 
-	* WeightedQuickUnionUF will be only attached to the top site, and
-	* will prevent backwash by never being attached to the virtualBottom,
-	* therefore stopping all the open bottom sites from being unionized
-	* to the other open bottom sites through the virtualBottom site.
-	*/ 
-		quickUnion = new WeightedQuickUnionUF(totSites + 3); 		// three are added to include the virtual sites
-		backWash = new WeightedQuickUnionUF(totSites + 1);		// one extra to have sites be between 0 to N-1
-	}
-
-
+	   if (N <= 0) throw new IllegalArgumentException("N must be more than zero");
+	
+	   gridSize = N;
+	   numOfOpenSites = 0;
+		// creates grid N x N
+	   grid = new boolean[N][N];
+	   
+	   // initialize top and bottom virtual sites
+	   VIRTUAL_TOP = 0;
+	   VIRTUAL_BOTTOM = ((N) * (N))+ 1;
+	 
+	   
+	   // initialize WeightedQuickUnionUF objects
+	   uf = new WeightedQuickUnionUF(N * N + N);
+	   backwash = new WeightedQuickUnionUF(N* N + N);
+	   
+	   }
+	   
+	
 	/**
-	 * Opens site (row i, column j) if it is not open already opened
-	 * 
-	 * @param i - row number
-	 * @param j - column number
+	 * Opens site(row i, column j) if it is not open already.
+	 * @param if site(row i, column j) is open.
+	 * @param i row of the grid
+	 * @param j j column of the grid
 	 */
 	public void open(int i, int j) {
-		checkIfOutOfBounds(i, j);					// calls the method created to check if out of bounds
-
-		if (!isOpen(i, j)) { 						// if the site is not already open, then open it
-			isGridSiteOpen[i][j] = true; 				// sets the boolean site to true, signifying the spot as open
-			openSites++;						// increments openSites to simplify numberOfOpenSites() method
-			unionizeSurroundingSides(i, j); 			// unionizes the one site to all the surrounding ones
-		}
-	}
-
+		
+		checkIfOutOfBounds(i,j);
+		
+		if(!isOpen(i,j)) {
+			//set cell to open
+			grid[i][j]= true;
+			numOfOpenSites++;
+			//unionize surrounding sides
+			unionizeSurroundingSides(i,j);
+		}	
+	}		
+	
 	/**
-	 * Checks if site (row i, column j) is open
-	 * 
-	 * @param i - row number
-	 * @param j - column number
-	 * @return - whether the site spot is true or false, (open or unopen)
+	 * Checks if site(row i, column j) is open.
+	 * @param i row of the grid
+	 * @param j column of the grid
+	 * @return
 	 */
 	public boolean isOpen(int i, int j) {
-		checkIfOutOfBounds(i, j); 						// calls the method created to check if out of bound
-		return isGridSiteOpen[i][j]; 						// returns whether the boolean grid spot is open or not
+		checkIfOutOfBounds(i,j);
+		return grid[i][j];
+		
 	}
-
+	
 	/**
-	 * Checks if site (row i, column j) is full 
-	 * full site is an open site that can be connected to the top row
-	 * via a chain of neighboring (left, right, up,
-	 * down) open sites.
-	 * 
-	 * @param i - row number
-	 * @param j - column number
-	 * @return - whether the site is full or not
+	 * Checks if site (row i, column j) is full.
+	 * @param i row of the grid
+	 * @param j column of the grid
+	 * @return
 	 */
 	public boolean isFull(int i, int j) {
-		checkIfOutOfBounds(i, j); 						// calls the method created to check if out of bound
-		return backWash.connected(xyToOneD(i, j), virtualTop);	
-		//uses the backWash WeightedQuickUnionUf since this one isn't attached to the virtualBottom
+		checkIfOutOfBounds(i, j);
+		if(isOpen(i,j)) {
+			int index = xyToOneD(i,j);
+			return backwash.connected(index, VIRTUAL_TOP);
+		}else 
+			return false;
+		
 	}
-
+	
 	/**
-	 * Checks if the system percolates
-	 * The system percolates when the bottom and the 
-	 * top are connected.
-	 * 
-	 * @return whether the N-by-N grid is connected from the bottom to the top
+	 * Checks whether the system percolates.
+	 * @return
 	 */
 	public boolean percolates() {
-		return quickUnion.connected(virtualBottom, virtualTop);
+		return uf.connected(VIRTUAL_TOP, VIRTUAL_BOTTOM);
+		
 	}
-
+	
+	private int xyToOneD(int i, int j) {
+        if (i < 0 || i > gridSize || j < 0 || j > gridSize)
+            throw new IndexOutOfBoundsException("Illegal parameter value.");
+        return ((i * gridSize ) + j + 1) ; 
+    }
 	/**
-	 * Unionizes the grid site to the surrounding sides when the site is opened
-	 * @param i - row number
-	 * @param j - column number
+	 * 
+	 * @param i row of the grid
+	 * @param j column of the grid
 	 */
 	private void unionizeSurroundingSides(int i, int j) {
-		//sets the site given to a One dimensional number
-		int gridSpotOneDNum = xyToOneD(i, j);
+	int gridSpotOneDNum = xyToOneD(i, j);
+	
 
-		//UNIONIZING TOP
-		if (i == 0) {				// this would be the top row, so we attach it to the virtual top
-			quickUnion.union(gridSpotOneDNum, virtualTop);
-			backWash.union(gridSpotOneDNum, virtualTop);}	
-		else { 						
-			if (isOpen(i-1, j)) {		//unionizing this site to the site above if it is already open
-				quickUnion.union(gridSpotOneDNum, xyToOneD(i - 1, j));
-				backWash.union(gridSpotOneDNum, xyToOneD(i - 1, j));} 
-		}
+		if (i == 0) { // union virtual top
+			uf.union(VIRTUAL_TOP,gridSpotOneDNum);
+			backwash.union(VIRTUAL_TOP,gridSpotOneDNum);
+        }
+        if (i == (gridSize-1)) { // union virtual bottom
+            
+        	uf.union(gridSpotOneDNum, VIRTUAL_BOTTOM);
+        }
 
-		//UNIONIZING BOTTOM
-		if (i == LENGTH-1) {			// this would be the bottom row, so we attach it to the virtual bottom
-			quickUnion.union(gridSpotOneDNum, virtualBottom);} //we exclude the backWash from unionizing with the virtualBottom	
-		else { 
-			if (isOpen(i+1, j)) {		//unionizing this site to the site below if it is open
-				quickUnion.union(gridSpotOneDNum, xyToOneD(i + 1, j));
-				backWash.union(gridSpotOneDNum, xyToOneD(i + 1, j));
-			}
-		}
+        // union possible neighbor(s)
+        if (i > 0 && isOpen(i - 1, j)) {
+            uf.union(gridSpotOneDNum, xyToOneD(i - 1, j));
+            backwash.union(gridSpotOneDNum, xyToOneD(i - 1, j));
+            
+        }
+        if (i < (gridSize-1) && isOpen(i + 1, j)) {
+            uf.union(gridSpotOneDNum, xyToOneD(i + 1, j));
+            backwash.union(gridSpotOneDNum, xyToOneD(i + 1, j));
+        }
+        if (j > 0 && isOpen(i, j - 1)) {
+            uf.union(gridSpotOneDNum, xyToOneD(i, j - 1));
+            backwash.union(gridSpotOneDNum, xyToOneD(i, j - 1));
+        }
+        if (j < (gridSize-1) && isOpen(i, j + 1)) {
+            uf.union(gridSpotOneDNum, xyToOneD(i, j + 1));
+            backwash.union(gridSpotOneDNum, xyToOneD(i, j + 1));
+        }
 		
-		//UNIONIZING RIGHT
-		if (j < LENGTH-1 && isOpen(i, j+1)) {	// attaching the right side, if it isn't the right edge and is already open
-			quickUnion.union(gridSpotOneDNum, xyToOneD(i, j + 1));
-			backWash.union(gridSpotOneDNum, xyToOneD(i, j + 1));}
-		
-		//UNIONIZING LEFT
-		if (j > 0 && isOpen(i, j-1)) {	//attaching the left side, if it isn't the left edge and is already open
-			quickUnion.union(gridSpotOneDNum, xyToOneD(i, j - 1));
-			backWash.union(gridSpotOneDNum, xyToOneD(i, j - 1));}
 	}
-
-	/**
-	 * Changes the 2D grid numbers to a 1D number 
-	 * if (0,0) - num is 0 
-	 * if (0,1) - num is 1 
-	 * if (1,0) - num is 10 
-	 * if (1,1) - num is 11
-	 * 
-	 * @param i - row number
-	 * @param j - column number
-	 * @return corresponding number to the 2d grid
-	 */
-	private int xyToOneD(int i, int j) {
-		return (LENGTH * i) + j;
-	}
-
+	
+	
 	/**
 	 * Simple method to check if the isOpen(), isFull(), or Open() is out of bounds
-	 * 
-	 * @param i - row number
-	 * @param j - column number
+	 * @param i  row of the grid
+	 * @param j column of the grid
 	 */
 	private void checkIfOutOfBounds(int i, int j) {
-		if (i < 0 || i >= LENGTH)
-			throw new IndexOutOfBoundsException("row index " + i + " must be between 0 and " + (LENGTH - 1));
-
-		if (j < 0 || j >= LENGTH)
-			throw new IndexOutOfBoundsException("column index " + j + " must be between 0 and " + (LENGTH - 1));
+		if (i < 0 || i >= gridSize) throw new IndexOutOfBoundsException
+		("row index " + i + " must be between 0 and " + (gridSize-1));
+		
+		if (j < 0 || j >= gridSize) throw new IndexOutOfBoundsException
+		("column index " + j + " must be between 0 and " + (gridSize-1));
 	}
-
-	private int numberOfOpenSites() {
-		return openSites;
+	/**
+	 * Calculates number of opened sites
+	 * @return numberOfOpenSites
+	 */
+	public int numberOfOpenSites() {
+		return numOfOpenSites;
 	}
-
-}
+	
+	}
+	
