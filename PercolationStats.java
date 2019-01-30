e65a7ce6f66db79bccbf265e3e4eff490df6067e
annieruiz <45504443+annieruiz@users.noreply.github.com>
2019-01-30 13:31:13 -0700

    Finished writing program
    Added comments and finished the TODOs, code should be fully running and functioning now.

--- PercolationStats.java
+++ PercolationStats.java
@@ -3,25 +3,48 @@ package a01;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private int gridSize; //number of rows and columns in the N-by-N grid
-	private int timesExperimentRuns; //Amount of times the experiment will be run
-	private double[] results; //array that stores the results of the experiments
+	//made final because they never change once implemented
+	private final int GRID_SIZE; //number of rows and columns in the N-by-N grid
+	private final int TIMES_EXPERIMENT_RUNS; //Amount of times the experiment will be run
+	
+	private double[] percolationThreshold; //array that stores the results of the experiments
 
 	/**
-	 * Performs T independent experiments on an N­by­N grid
+	 * Performs T independent experiments on an NÂ­byÂ­N grid
 	 * 
-	 * @param N
-	 * @param T
+	 * @param N - number of columns/rows in the grid
+	 * @param T - number of times to run stats
 	 */
 	public PercolationStats(int N, int T) {
+		//throws illegal argument if the numbers given are less than or equal to 0
 		if (N <= 0 || T <= 0) throw new IllegalArgumentException("Given N or T are less than or equal to 0(PercolationStats())");
 		
-		timesExperimentRuns= T;
-		results = new double[T];
+		TIMES_EXPERIMENT_RUNS= T;					//stores number of times experiment runs as global variable
+		percolationThreshold = new double[TIMES_EXPERIMENT_RUNS];  	//stores results of percolation experiment
+		int row;
+		int column;
+		GRID_SIZE = N * N;						//stores the grid size
 		
-		//running the percolation experiment T number of times
-		for (int i = 0; i < timesExperimentRuns; i++) {
-			Percolation p = new Percolation(N);
+		for (int i = 0; i < timesExperimentRuns; i++) {			//running the percolation experiment T number of times
+			Percolation p = new Percolation(N);			//creates a new Percolation class with an N-by-N grid
+			while(!p.percolates()) { 				//randomly opens sites until the system percolates,
+				row = StdRandom.uniform(0, N);			//sets row to a random number
+				column = StdRandom.uniform(0, N);		//sets column to a random number
+				
+				/*if the site is not already open, then we open the site
+				* and increment the openedSites variable to keep track of
+				* how many opened sites there are
+				*/
+				if(!p.isOpen(row, column)) { 
+					p.open(row, column);
+				}
+			}
+			
+			/*sets the percolationThreshold array to what the threshold was for this experiment
+			* in order to keep track of the threshold for each experiment to later get the
+			* statistics of the tests ran
+			*/
+			percolationThreshold[i] = (double) p.numberOfOpenSites() / (double) GRID_SIZE;
 		}
 	}
 
@@ -30,7 +53,7 @@ public class PercolationStats {
 	 * @return the mean of percolation threshold
 	 */
 	public double mean() {
-		return StdStats.mean(results);
+		return StdStats.mean(percolationThreshold);
 	}
 
 	/**
@@ -38,18 +61,16 @@ public class PercolationStats {
 	 * @return the sample standard deviation of the percolation threshold
 	 */
 	public double stddev() {
-		if (StdStats.stddev(results) == 1) return Double.NaN; //if the result equals one, then it returns that the double is not a number
-		return StdStats.stddev(results);
+		if (StdStats.stddev(percolationThreshold) == 1) return Double.NaN; //if the result equals one, then it returns that the double is not a number
+		return StdStats.stddev(percolationThreshold);
 	}
 
 	/**
 	 * Finds the low endpoint of 95% confidence interval
-	 * 
 	 * @return the low endpoint of the 95% confidence interval
 	 */
 	public double confidenceLow() {
-		return 0d;
-		// TODO
+		return mean() - ((1.96*stddev())/Math.sqrt(TIMES_EXPERIMENT_RUNS));
 	}
 
 	/**
@@ -58,7 +79,6 @@ public class PercolationStats {
 	 * @return
 	 */
 	public double confidenceHigh() {
-		return 0d;
-		// TODO
+		return mean() + ((1.96*stddev())/Math.sqrt(TIMES_EXPERIMENT_RUNS));
 	}
 }
