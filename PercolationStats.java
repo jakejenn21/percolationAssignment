55eb487da08de7ae2aeac8f5feb82cdea1ce4694
jakejenn21 <47189949+jakejenn21@users.noreply.github.com>
2019-01-30 14:16:46 -0700

    Update PercolationStats.java

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,50 +1,31 @@
 package a01;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	//made final because they never change once implemented
-	private final int GRID_SIZE; //number of rows and columns in the N-by-N grid
-	private final int TIMES_EXPERIMENT_RUNS; //Amount of times the experiment will be run
-	
-	private double[] percolationThreshold; //array that stores the results of the experiments
+	private int gridSize; //number of rows and columns in the N-by-N grid
+	private int timesExperimentRuns; //Amount of times the experiment will be run
+	private double[] results; //array that stores the results of the experiments
 
 	/**
 	 * Performs T independent experiments on an N­by­N grid
 	 * 
-	 * @param N - number of columns/rows in the grid
-	 * @param T - number of times to run stats
+	 * @param N - gridSize
+	 * @param T - timesExperimentRuns
 	 */
 	public PercolationStats(int N, int T) {
-		//throws illegal argument if the numbers given are less than or equal to 0
-		if (N <= 0 || T <= 0) throw new IllegalArgumentException("Given N or T are less than or equal to 0(PercolationStats())");
+		if (N <= 0 || T <= 0) throw new IllegalArgumentException(
+				"Given N or T are less than or equal to 0(PercolationStats())");
+		timesExperimentRuns= T;
+		gridSize = N;
+		results = new double[timesExperimentRuns];
 		
-		TIMES_EXPERIMENT_RUNS= T;					//stores number of times experiment runs as global variable
-		percolationThreshold = new double[TIMES_EXPERIMENT_RUNS];  	//stores results of percolation experiment
-		int row;
-		int column;
-		GRID_SIZE = N * N;						//stores the grid size
-		
-		for (int i = 0; i < timesExperimentRuns; i++) {			//running the percolation experiment T number of times
-			Percolation p = new Percolation(N);			//creates a new Percolation class with an N-by-N grid
-			while(!p.percolates()) { 				//randomly opens sites until the system percolates,
-				row = StdRandom.uniform(0, N);			//sets row to a random number
-				column = StdRandom.uniform(0, N);		//sets column to a random number
-				
-				/*if the site is not already open, then we open the site
-				* and increment the openedSites variable to keep track of
-				* how many opened sites there are
-				*/
-				if(!p.isOpen(row, column)) { 
-					p.open(row, column);
-				}
-			}
+		//running the percolation experiment T number of times
+		for (int i = 0; i < timesExperimentRuns; i++) {
+			results[i] = findPercolationThreshold();
 			
-			/*sets the percolationThreshold array to what the threshold was for this experiment
-			* in order to keep track of the threshold for each experiment to later get the
-			* statistics of the tests ran
-			*/
-			percolationThreshold[i] = (double) p.numberOfOpenSites() / (double) GRID_SIZE;
 		}
 	}
 
@@ -53,7 +34,7 @@ public class PercolationStats {
 	 * @return the mean of percolation threshold
 	 */
 	public double mean() {
-		return StdStats.mean(percolationThreshold);
+		return StdStats.mean(results);
 	}
 
 	/**
@@ -61,16 +42,18 @@ public class PercolationStats {
 	 * @return the sample standard deviation of the percolation threshold
 	 */
 	public double stddev() {
-		if (StdStats.stddev(percolationThreshold) == 1) return Double.NaN; //if the result equals one, then it returns that the double is not a number
-		return StdStats.stddev(percolationThreshold);
+		if (StdStats.stddev(results) == 1) return Double.NaN;
+		return StdStats.stddev(results);
 	}
 
 	/**
 	 * Finds the low endpoint of 95% confidence interval
+	 * 
 	 * @return the low endpoint of the 95% confidence interval
 	 */
 	public double confidenceLow() {
-		return mean() - ((1.96*stddev())/Math.sqrt(TIMES_EXPERIMENT_RUNS));
+		return mean() - 1.96 * stddev() / Math.sqrt(results.length);
+		// TODO
 	}
 
 	/**
@@ -79,6 +62,54 @@ public class PercolationStats {
 	 * @return
 	 */
 	public double confidenceHigh() {
-		return mean() + ((1.96*stddev())/Math.sqrt(TIMES_EXPERIMENT_RUNS));
+		return mean() + 1.96 * stddev() / Math.sqrt(results.length);
+		// TODO
 	}
+	
+	/**
+	 * Calculates percolation threshold for each experiment
+	 * @return percolationThreshold
+	 */
+	private double findPercolationThreshold() {
+	    Percolation perc = new Percolation(gridSize);
+	    int i, j;
+	    int count = 0;
+	    while (!perc.percolates()) {
+	      do {
+	        i = StdRandom.uniform(gridSize) ;
+	        j = StdRandom.uniform(gridSize) ;
+	      } while (perc.isOpen(i,j));
+	      count++;
+	      perc.open(i, j);
+	    }
+	    return count/(Math.pow(gridSize ,2));
+	  }
+	
+	public static void main(String[] args) {
+		//TESTING
+	    PercolationStats stats = new PercolationStats(200, 50);
+	    StdOut.printf("mean = %f\n", stats.mean());
+	    StdOut.printf("stddev = %f\n", stats.stddev());
+	    StdOut.printf("confidenceLow = %f\n", stats.confidenceLow());
+	    StdOut.printf("confidenceHigh = %f\n", stats.confidenceHigh());
+	    
+	    StdOut.println();
+	    
+	    PercolationStats stats1 = new PercolationStats(200, 100);
+	    StdOut.printf("mean = %f\n", stats1.mean());
+	    StdOut.printf("stddev = %f\n", stats1.stddev());
+	    StdOut.printf("confidenceLow = %f\n", stats1.confidenceLow());
+	    StdOut.printf("confidenceHigh = %f\n", stats1.confidenceHigh());
+	    
+	    StdOut.println();
+	    
+	    PercolationStats stats2 = new PercolationStats(500, 120);
+	    StdOut.printf("mean = %f\n", stats2.mean());
+	    StdOut.printf("stddev = %f\n", stats2.stddev());
+	    StdOut.printf("confidenceLow = %f\n", stats2.confidenceLow());
+	    StdOut.printf("confidenceHigh = %f\n", stats2.confidenceHigh());
+	    
+	    
+	}
+
 }
